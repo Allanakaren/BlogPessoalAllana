95062a21ce0f7073165aecd4b10c54b268cb8438
allana karen <lannaeliege@gmail.com>
2022-06-05 18:00:30 -0300

    complementando o inicio

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-inicio',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
 
-  constructor() { }
+  constructor( 
+    private router: Router
+
+
+  ){ }
 
   ngOnInit(): void {
+
+  if(environment.token ==''){
+    /*alert('sua seção expirou façao login novamente.')*/
+    this.router.navigate(['/entrar'])
+
+  }
+
   }
 
 }
