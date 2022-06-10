b084c46b6d24af863fed81686827206b62eeab18
allana karen <lannaeliege@gmail.com>
2022-06-09 21:37:51 -0300

    implementando o post em postagem

---
+++ src/app/service/postagem.service.ts
@@ -0,0 +1,42 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+import { Postagem } from '../model/Postagem';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostagemService {
+  
+  constructor(private http: HttpClient) { }
+
+  token ={
+    headers: new HttpHeaders().set('Authorization', environment.token)
+  }
+
+  getAllPostagens(): Observable<Postagem[]>{
+    return this.http.get<Postagem[]>('https://blogpessoalallanakaren.herokuapp.com/postagens', this.token)
+
+  }
+  
+  getByIdPostagem(id: number): Observable<Postagem>{
+    return this.http.get<Postagem>(`https://blogpessoalallanakaren.herokuapp.com/postagens/${id}`, this.token)
+  }
+
+  PostPostagem(Postagem: Postagem): Observable<Postagem>{
+     return this.http.post<Postagem>('https://blogpessoalallanakaren.herokuapp.com/postagens', Postagem, this.token)
+
+  }
+
+  putPostagem(Postagem: Postagem): Observable<Postagem>{
+    return this.http.put<Postagem>('https://blogpessoalallanakaren.herokuapp.com/postagens', Postagem, this.token)
+  }
+
+  deletePostagem(id: number){
+     return this.http.delete(`https://blogpessoalallanakaren.herokuapp.com/postagens/${id}`, this.token)
+  }
+
+
+
+}
